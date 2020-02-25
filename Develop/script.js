696faa4030051d26fb73eccc299d2a612e8b4d18
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-02-25 18:50:39 -0500

    Added date fxn & base styling

--- Develop/script.js
+++ Develop/script.js
@@ -3,12 +3,22 @@ var busHours = ["9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm"
 
 // Display timeblocks for standard business hours (9am to 5pm)
 $(document).ready(function() {
+
+    // Current day displayed at top of calendar
+    var todaysDate = new Date();
+    var year = todaysDate.getFullYear();
+    var month = todaysDate.getMonth() + 1;
+    var day = todaysDate.getDate();
+    console.log(month, day, year);
+    $("#currentDay").text(month + " / " + day + " / " + year);
+
+    // Each timeblock has input field & save button
     for (var i = 0; i < busHours.length; i++) {
-        // Make a Row
+        // Generate a Row
         var newRow = $("<div>");
         newRow.addClass("row");
 
-    // Make 3 columns with classes: col-1, col-10, col-1
+    // Generate 3 columns with classes: col-1, col-10, col-1
         // Hour col-1
         var hourCol = $("<div>");
         hourCol.addClass("col-1 hour");
@@ -20,7 +30,7 @@ $(document).ready(function() {
         var saveCol = $("<div>");
         saveCol.addClass("col-1 saveBtn");
 
-        // Apply the text
+        // Apply the labels
         hourCol.text(busHours[i]);
         textSpot.text("");
         saveCol.text("Save");
@@ -32,10 +42,8 @@ $(document).ready(function() {
     };
 });
 
-// Each timeblock has input field & save button
+
 
 // Save button stores input text to localStorage, so it stays even when refreshed.
-
-// Current day displayed at top of calendar
 
 // Timeblock is colored based on Past, Present, or Future
