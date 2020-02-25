d7ada28db6948c1237aa934faa3b55206c31f6de
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-02-24 22:35:32 -0500

    asdfsdf

---
+++ Develop/script.js
@@ -0,0 +1,11 @@
+
+
+// Display timeblocks for standard business hours (9am to 5pm)
+
+// Each timeblock has input field & save button
+
+// Save button stores input text to localStorage, so it stays even when refreshed.
+
+// Current day displayed at top of calendar
+
+// Timeblock is colored based on Past, Present, or Future
