c07ce61cc78892f08e32b53d2987d93a5f2b2e64
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-02-26 01:06:42 -0500

    First truly responsive webpage
    dem transitions doh

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 
 // Display timeblocks for standard business hours (9am to 5pm)
 $(document).ready(function() {
-    var busyHours = ["9 am", "10 am", "11 am", "12 am", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm"];
+    var busyHours = ["8 am", "9 am", "10 am", "11 am", "12 am", "1 pm", "2 pm", "3 pm", "4 pm", "5 pm", "6 pm", "7 pm", "8 pm"];
 
     // Current day displayed at top of calendar
     var todaysDate = new Date();
@@ -39,7 +39,7 @@ $(document).ready(function() {
         saveCol.addClass("col-1 saveBtn");
         var saveP = $("<p>");
         var saveIcon = $("<i>");
-        saveIcon.addClass("fas fa-save");
+        saveIcon.addClass("fa fa-check");
          
 
         // Apply the labels
