c25e0739b06616950a68f5d5c1880c874dc43196
apatel922 <59633697+apatel922@users.noreply.github.com>
2020-02-25 20:55:28 -0500

    Assigned an incrementing ID to each generated textarea

--- Develop/script.js
+++ Develop/script.js
@@ -1,19 +1,19 @@
 
-var busHours = ["9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
 // Display timeblocks for standard business hours (9am to 5pm)
 $(document).ready(function() {
+    var busyHours = ["9am", "10am", "11am", "12am", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
     // Current day displayed at top of calendar
     var todaysDate = new Date();
     var year = todaysDate.getFullYear();
     var month = todaysDate.getMonth() + 1;
     var day = todaysDate.getDate();
-    console.log(month, day, year);
+    // console.log(month, day, year);
     $("#currentDay").text(month + " / " + day + " / " + year);
 
     // Each timeblock has input field & save button
-    for (var i = 0; i < busHours.length; i++) {
+    for (var i = 0; i < busyHours.length; i++) {
         // Generate a Row
         var newRow = $("<div>");
         newRow.addClass("row");
@@ -24,14 +24,22 @@ $(document).ready(function() {
         hourCol.addClass("col-1 hour");
         // Entry Field col-10
         var entryCol = $("<div>");
+        entryCol.addClass("col-10 present");
+        // Textarea child 
         var textSpot = $("<textarea>");
-        entryCol.addClass("col-10");
+        textSpot.attr("id", i+1);
+        console.log(textSpot);
+       
+    // IF - ELSE for col-10 Timeblock color
+        // IF - hour is earlier than present, remove present & add past
+        // ELSE - hour is later than present, remove present & add future
+
         // Save Button col-1
         var saveCol = $("<div>");
         saveCol.addClass("col-1 saveBtn");
 
         // Apply the labels
-        hourCol.text(busHours[i]);
+        hourCol.text(busyHours[i]);
         textSpot.text("");
         saveCol.text("Save");
 
@@ -40,10 +48,21 @@ $(document).ready(function() {
         $(newRow).append(hourCol, entryCol, saveCol);
         $(entryCol).append(textSpot);
     };
+
+    // Save button stores input text to localStorage, so it stays even when refreshed.
+    var genericArea = $("textarea");
+
+    var onRefresh = localStorage.getItem("input");
+    genericArea.textContent = onRefresh;
+
+    $(".saveBtn").click(function() {
+        var thisTextArea = $(this).siblings(".col-10").find(genericArea);
+        var textVal = thisTextArea.val();
+        console.log(textVal);
+        thisTextArea.textContent = onRefresh;
+
+        localStorage.setItem("input", textVal);
+    });
 });
 
 
-
-// Save button stores input text to localStorage, so it stays even when refreshed.
-
-// Timeblock is colored based on Past, Present, or Future
